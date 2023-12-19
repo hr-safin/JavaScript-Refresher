4b8784b764a22295cdc98340b4eb6b476d22faf4
safinhr <hrsafin2434@gmail.com>
2023-12-19 20:56:00 +0600

    array method : concat() & push()

--- arrayMethod.js
+++ arrayMethod.js
@@ -24,3 +24,15 @@ let data3 = language.slice(1,3)
 // let data4 = language.splice(1,2, "Ruby", "React JS", "Next JS")
 // console.log(data4)
 // console.log(language)
+
+
+// concat method don't change the original array and return new new array
+
+let data5 = language.concat("lang1", "lang2")
+
+// push method change the original  array
+
+let data6 =  language.push("lang3")
+
+
+
