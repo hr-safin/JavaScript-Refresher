5c65c12d7107d8de5d6cf2c10370848508a17ccc
safinhr <hrsafin2434@gmail.com>
2023-12-19 14:51:31 +0600

    js: event handler refresher

---
+++ event.js
@@ -0,0 +1,12 @@
+// select
+const myButton = document.getElementById("btn")
+
+
+// event handler
+
+function hello(){
+    console.log("Hello i am revising javascript")
+}
+
+
+myButton.addEventListener("click", hello)
