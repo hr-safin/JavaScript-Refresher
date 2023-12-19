82954672088d0e20de8d16f53a86aacfb10b76c2
safinhr <hrsafin2434@gmail.com>
2023-12-19 21:22:58 +0600

    array method : map()

--- arrayMethod.js
+++ arrayMethod.js
@@ -1,5 +1,5 @@
 // array method
-let language = ["Python", "C++", "Go", "C#", "Javascript" ]
+let language = ["Python", "C++", "Go", "C#", "Javascript", "Javascript" ]
 
 // find method
 
@@ -28,11 +28,40 @@ let data3 = language.slice(1,3)
 
 // concat method don't change the original array and return new new array
 
-let data5 = language.concat("lang1", "lang2")
+// let data5 = language.concat("lang1", "lang2")
 
 // push method change the original  array
 
-let data6 =  language.push("lang3")
+// let data6 =  language.push("lang3")
 
+
+// map method
+// let language = ["Python", "C++", "Go", "C#", "Javascript", "Javascript" ]
+let newLanguage = []
+for(let i =0; i<language.length; i++){
+    if(language[i] === "Javascript"){
+        newLanguage.push("Javascript")
+    }else{
+        newLanguage.push("N/A")
+    }
+}
+
+// console.log(newLanguage)
+// console.log(language)
+
+let result = language.map(item => {
+    if(item === "Javascript"){
+        return "Javascript"
+    }else{
+        return "N/A"
+    }
+})
+
+// console.log(result)
+// console.log(language)
+
+let result2 = language.map(item => `${item}:`)
+
+// console.log(result2)
 
 
