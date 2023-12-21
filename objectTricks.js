76060bd8a30fbe686fd116ac5a39ab2fedfd25fb
safinhr <hrsafin2434@gmail.com>
2023-12-21 10:21:39 +0600

    object tricks

---
+++ objectTricks.js
@@ -0,0 +1,13 @@
+
+const information = {
+    name : "Hasibur Rahman Safin",
+    id : 21301585,
+    department : "CSE",
+    goal : "full stack software engineer",
+    cricket,
+
+}
+
+
+
+console.log(information)
