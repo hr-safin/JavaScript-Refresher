d9d119ce7f54cd6d87682bfa6f2de93269fd7a03
safinhr <hrsafin2434@gmail.com>
2023-12-21 22:25:40 +0600

    falsy and truthy value

---
+++ truthyOrFalsy.js
@@ -0,0 +1,11 @@
+// falsy value > false, 0, empty string "", null , undefined, NaN
+
+const result = ""
+
+if(result){
+    console.log("This is true")
+}
+
+else{
+    console.log("This is false")
+}
