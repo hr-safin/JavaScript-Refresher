cc9b61e4ad10ed4035fa5339292e36ecc09e270f
safinhr <hrsafin2434@gmail.com>
2024-04-24 19:40:51 +0600

    truthy & falsy values

---
+++ 6NOTOperator.js
@@ -0,0 +1,17 @@
+
+// let price = 500
+
+// if(!price === 500){
+//     console.log("You do not have enough money to buy this chocolate")
+// }
+// else{
+//     console.log("You have enough money to buy this chocolate")
+// }
+
+if(''){
+    console.log("True")
+}
+
+else{
+    console.log("False")
+}
