47faed79f4c120b6243271a3241038b43d58c0af
safinhr <hrsafin2434@gmail.com>
2024-04-29 20:39:33 +0600

    parameter vs arguments in js

---
+++ 12Function/12ParameterVSArguments.js
@@ -0,0 +1,7 @@
+// parameters are like placeholder they do not hold the actual value
+function addition(x,y){
+    console.log(x + y)
+}
+
+// arguments are when you provide actual value to the parameters
+addition(34,34)
