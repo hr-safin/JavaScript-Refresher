7c43da9a48528a28c433fd43f790c26c35b569b0
safinhr <hrsafin2434@gmail.com>
2024-07-06 20:39:22 +0600

    find, filter, map, forEach method

--- RevisonMainTopic/ES6-JAVASCRIPT/ImportantTopic.js
+++ RevisonMainTopic/ES6-JAVASCRIPT/ImportantTopic.js
@@ -24,3 +24,43 @@ const arr = ["one", "two", "three"]
 const [first, second, third] = arr
 
 console.log(first, second, third)
+
+
+// filter method
+
+const arrayOfNumber = [1,2,3,4,5,6,7]
+
+const evenNumber = arrayOfNumber.filter(num => num % 2 === 0)
+
+console.log(evenNumber)
+
+
+// forEach method
+
+const fruits = ["apple", "Banana", "mango"]
+
+fruits.forEach(fruit => console.log(fruit))
+
+for(let fruit of fruits){
+    console.log(fruit)
+}
+
+for(let i =0; i<fruits.length; i++){
+    console.log(fruits[i])
+}
+
+// find method
+
+const numbers = [1, 2, 3, 4,5,6]
+
+const  numb = numbers.find( num =>  num % 2 === 0)
+
+console.log(numb)
+
+// map
+
+const numbers2 = [1,2,3,4,5,6]
+
+const newArray = numbers2.map(num =>  num *2)
+
+console.log(newArray)
