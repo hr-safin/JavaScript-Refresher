e16638322de9645a0d086ab58f3aaad3a4a50b1c
safinhr <hrsafin2434@gmail.com>
2024-07-06 20:27:07 +0600

    destructuring for array and object

--- RevisonMainTopic/ES6-JAVASCRIPT/ImportantTopic.js
+++ RevisonMainTopic/ES6-JAVASCRIPT/ImportantTopic.js
@@ -5,3 +5,22 @@ const userName = (user) => {
 }
 
 userName("Hasibur Rahman Safin")
+
+
+// Destructuring for array and object
+
+const person = {
+    name1 : "Hasibur Rahman Safin",
+    school : "New Jersey Institute of Technology",
+}
+
+const {name1, school} = person
+
+console.log(name1)
+console.log(school)
+
+const arr = ["one", "two", "three"]
+
+const [first, second, third] = arr
+
+console.log(first, second, third)
